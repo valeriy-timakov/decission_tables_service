5fddd1ec62b243ce4a22c185f8119b4536663c99
Valerii <valtimakov@gmail.com>
2025-02-02 12:06:13 +0200

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
 extern crate core;
 
+use simd_json::borrowed::Value as JsonValue;
+use simd_json::prelude::ValueAsScalar;
+
 fn main() {
     println!("Hello, world!");
-    // let c = C::new();
-    // let d = D::new();
-    // c.do_something();
-    // d.do_something();
-    // call_do_something(c);
-    // call_do_something(d);
+
+    let mut data = br#"{
+        "parameters": {
+            "name": "someName",  
+            "age": 23      
+        }
+    }"#.to_vec();
+
+    // Парсимо JSON
+    let mut parsed: JsonValue = simd_json::from_slice(&mut data).expect("Failed to parse JSON");
+
+    // Дістаємо значення "age"
+    if let Some(age) = parsed["parameters"]["age"].as_i64() {
+        println!("Age: {}", age);
+    } else {
+        println!("Field 'age' not found or not an integer");
+    }
+    
 }
 
 trait Condition<T: PartialEq + PartialOrd> {
@@ -36,9 +51,24 @@ struct Rule<T: PartialEq + PartialOrd> {
 }
 
 impl<T: PartialEq + PartialOrd> Rule<T> {
-    fn check(&self, index: usize, value: &T) -> bool {
+    fn checkOne(&self, index: usize, value: &T) -> bool {
         self.condition.check(&self.values[index], &value)
     }
 }
+
+impl<'a, T: PartialEq + PartialOrd + TryFrom<JsonValue<'a>, Error = String>> Rule<T> {
+    fn checkAll(&self, index: usize, value: &JsonValue<'a>) -> Result<Vec<usize>, String> {
+        let t_value = T::try_from(value.clone())?;
+        let mut result: Vec<usize> = Vec::new();
+        for (index, value) in self.values.iter().enumerate() {
+            if self.checkOne(index, &t_value) {
+                result.push(index);
+            }
+        }
+        Ok(result)
+    }
+}
+
+
 
 struct DecisionTable {}
