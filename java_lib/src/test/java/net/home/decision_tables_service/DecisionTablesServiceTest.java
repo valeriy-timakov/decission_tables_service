ce78b190d49944db84dea4503990d0c810c76723
Valerii <valtimakov@gmail.com>
2025-06-23 11:12:03 +0300

    ignore

--- java_lib/src/test/java/net/home/decision_tables_service/DecisionTablesServiceTest.java
+++ java_lib/src/test/java/net/home/decision_tables_service/DecisionTablesServiceTest.java
@@ -8,11 +8,11 @@ public class DecisionTablesServiceTest {
     @Test
     public void testCreateDecisionTable() {
         DecisionTablesService bridge = new DecisionTablesService();
-        int idx = bridge.createDecisionTable("D:\\Downloads\\1\\EnterPrintouts_FullRights.xlsx", 6);
+        int idx = bridge.createDecisionTable("D:\\Downloads\\1\\Printouts.xlsx", 6);
         Assertions.assertEquals(0, idx);
         var request = """
             {
-                "salesProgramCode": 600004,
+                "salesProgramCode": 250004,
                 "acquisitionType": "NEW",
                 "creditContractYear": "Default",
                 "signatory": "AGENT",
@@ -22,7 +22,7 @@ public class DecisionTablesServiceTest {
         var response = bridge.executeRequest(0, request);
         Assertions.assertNotNull(response);
         Assertions.assertEquals(1, response.length);
-        Assertions.assertEquals("{\"Preamble\": \"empty\", \"SigningPerson\": \"empty\", \"Text\": \"empty\", \"Note\": \"empty\", \"Statement\": \"empty\", \"Notification\": \"casco_ARX_digital_notification_0001\"}", response[0]);
+        Assertions.assertEquals("{\"Preamble\": \"empty\", \"SigningPerson\": \"empty\", \"Text\": \"empty\", \"Note\": \"empty\", \"Statement\": \"empty\", \"Notification\": \"some_text_notification_template\"}", response[0]);
     }
 
 
@@ -30,7 +30,7 @@ public class DecisionTablesServiceTest {
     public static void main(String[] args) {
         var request = """
              {
-            "salesProgramCode": 600004,
+                "salesProgramCode": 250004,
                 "acquisitionType": "NEW",
                 "creditContractYear": "Default",
                 "signatory": "AGENT",
@@ -39,7 +39,7 @@ public class DecisionTablesServiceTest {
         }""";
         DecisionTablesService bridge = new DecisionTablesService();
         long start = System.currentTimeMillis();
-        int idx = bridge.createDecisionTable("D:\\Downloads\\1\\EnterPrintouts_FullRights.xlsx", 6);
+        int idx = bridge.createDecisionTable("D:\\Downloads\\1\\Printouts.xlsx", 6);
         System.out.println("Load time: " + (System.currentTimeMillis() - start));
         System.out.println("idx: " + idx); // 7
         try {
